0b5f19856b0694245b9d35cd39f635af44f465d1
Alberto Luti <betomdq.93@gmail.com>
2018-07-26 19:52:31 -0300

    added selection functionality to the league activity

--- app/src/main/java/com/example/betom/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/betom/swoosh/LeagueActivity.kt
@@ -4,8 +4,14 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
+
+    val NO_CHOISE = ""
+
+    var selectedLeague = NO_CHOISE
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,7 +19,42 @@ class LeagueActivity : BaseActivity() {
     }
 
     fun leagueNextClicked(view: View){
-        val skillActivity= Intent(this,SkillActivity::class.java)
-        startActivity(skillActivity)
+        if(selectedLeague != NO_CHOISE) {
+            val skillActivity = Intent(this, SkillActivity::class.java)
+            startActivity(skillActivity)
+        } else {
+            Toast.makeText(this,"please select a league.",Toast.LENGTH_SHORT).show()
+        }
     }
+
+    fun onMensClicked(view: View){
+        if(mensLeagueButton.isChecked) {
+            womensLeagueButton.isChecked = false
+            coEdLeagueButton.isChecked = false
+
+            selectedLeague = "mens"
+        }else
+            selectedLeague=NO_CHOISE
+    }
+
+    fun onWomensClicked(view: View){
+        if(womensLeagueButton.isChecked) {
+            mensLeagueButton.isChecked = false
+            coEdLeagueButton.isChecked = false
+
+            selectedLeague = "womens"
+        }else
+            selectedLeague=NO_CHOISE
+    }
+
+    fun onCoEdClicked(view: View){
+        if(coEdLeagueButton.isChecked) {
+            mensLeagueButton.isChecked = false
+            womensLeagueButton.isChecked = false
+
+            selectedLeague = "co-ed"
+        }else
+            selectedLeague=NO_CHOISE
+    }
+
 }
--- app/src/main/java/com/example/betom/swoosh/SkillActivity.kt
+++ app/src/main/java/com/example/betom/swoosh/SkillActivity.kt
@@ -3,7 +3,7 @@ package com.example.betom.swoosh
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class SkillActivity : AppCompatActivity() {
+class SkillActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
