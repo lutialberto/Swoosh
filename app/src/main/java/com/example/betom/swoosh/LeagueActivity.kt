337290b0d48d76387146b2a1726e5d451b773367
Alberto Luti <betomdq.93@gmail.com>
2018-07-26 18:58:21 -0300

    base activity added with 'LifeCycle' tag for log debug

--- app/src/main/java/com/example/betom/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/example/betom/swoosh/LeagueActivity.kt
@@ -3,7 +3,7 @@ package com.example.betom.swoosh
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class LeagueActivity : AppCompatActivity() {
+class LeagueActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
