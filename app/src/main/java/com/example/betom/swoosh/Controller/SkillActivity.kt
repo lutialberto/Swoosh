6b416271bd310e60715fffc1c38189808351a682
Alberto Luti <betomdq.93@gmail.com>
2018-07-26 21:47:48 -0300

    onSave and onRestore functions added to suport rotation in league and skill views

--- app/src/main/java/com/example/betom/swoosh/Controller/SkillActivity.kt
+++ app/src/main/java/com/example/betom/swoosh/Controller/SkillActivity.kt
@@ -2,6 +2,7 @@ package com.example.betom.swoosh.Controller
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.View
 import android.widget.Toast
 import com.example.betom.swoosh.Model.Player
@@ -14,10 +15,22 @@ class SkillActivity : BaseActivity() {
     val NO_SKILL_CHOISE = ""
     lateinit var player: Player
 
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putParcelable(EXTRA_PLAYER,player)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
         player = intent.getParcelableExtra(EXTRA_PLAYER)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        if(savedInstanceState != null) {
+            player= savedInstanceState.getParcelable(EXTRA_PLAYER)
+        }
     }
 
     fun onSkillFinishClicked(view: View){
